5952418a750e39e71926f5b71c4527f5b33e9657
Vezil <szymonwojaczek@wp.pl>
2025-01-31 17:40:18 +0100

    connect with edit endpoint

--- src/components/AddEditTrainee.vue
+++ src/components/AddEditTrainee.vue
@@ -6,52 +6,73 @@
                 <input v-model="trainee.firstName" placeholder="First Name" class="border p-2 rounded w-1/2" />
                 <input v-model="trainee.lastName" placeholder="Last Name" class="border p-2 rounded w-1/2" />
             </div>
-            <button @click="addUser" class="mt-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
+            <button
+                class="mt-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+                @click="operationType === 'add' ? addUser() : editUser()"
+            >
                 {{ baseCaption }}
             </button>
         </div>
         <div class="w-full md:w-1/3 bg-white mt-10 md:mt-0 p-6 rounded-lg shadow flex flex-col items-center">
             <img :src="photoUrl" class="w-24 h-24 rounded-full object-cover border" alt="User Photo" />
             <input type="file" @change="uploadPhoto" class="hidden" ref="fileInput" />
-            <button @click="selectPhoto" class="mt-4 bg-gray-500 text-white px-4 py-2 rounded">Change Photo</button>
+            <button @click="selectPhoto" class="mt-4 bg-gray-500 text-white px-4 py-2 rounded cursor-pointer">
+                Change Photo
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, computed, reactive } from 'vue';
+import { ref, computed, watch } from 'vue';
 import axios from 'axios';
 import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
 import { useTraineesStore } from '@/stores/trainees';
 
 const { editingTrainee, operationType } = storeToRefs(useTraineesStore());
 
-const firstName = ref('');
-const lastName = ref('');
 const fileInput = ref(null);
+const photoUrl = ref('');
+const router = useRouter();
 
 const trainee = computed(() => ({
-    firstName: operationType.value === 'add' ? firstName.value : editingTrainee.value.fullName.split(' ')[0],
-    lastName: operationType.value === 'add' ? lastName.value : editingTrainee.value.fullName.split(' ')[1]
+    firstName: operationType.value === 'add' ? '' : editingTrainee.value.fullName.split(' ')[0],
+    lastName: operationType.value === 'add' ? '' : editingTrainee.value.fullName.split(' ')[1]
 }));
 
-const photoUrl = computed(() => (operationType.value === 'add' ? '' : editingTrainee.value.avatar));
+const traineeToSend = computed(() => ({
+    name: `${trainee.value.firstName} ${trainee.value.lastName}`,
+    job: 'trainee'
+}));
 
 const baseCaption = computed(() => (operationType.value === 'add' ? 'Add User' : 'Edit User'));
 
+watch(
+    [operationType, editingTrainee],
+    () => {
+        photoUrl.value = operationType.value === 'add' ? '' : editingTrainee.value.avatar;
+    },
+    { immediate: true }
+);
+
 const addUser = async () => {
     try {
-        const traineeToSend = {
-            name:
-                operationType.value === 'add'
-                    ? `${firstName.value} ${lastName.value}`
-                    : `${editingTrainee.value.firstName} ${editingTrainee.value.lastName}`,
-            job: 'trainee'
-        };
-
-        await axios.post(`https://reqres.in/api/users`, traineeToSend);
+        await axios.post(`https://reqres.in/api/users`, traineeToSend.value);
     } catch (error) {
         console.error(error);
+    } finally {
+        router.push({ name: 'TraineeList' });
+    }
+};
+
+const editUser = async () => {
+    try {
+        await axios.put(`https://reqres.in/api/users/${editingTrainee.value.id}`, traineeToSend.value);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        router.push({ name: 'TraineeList' });
     }
 };
 
